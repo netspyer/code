81db72ffc2ae368624dbf35d83c467341ed06abb
Jian Yu <askingyj@gmail.com>
2015-12-02 14:38:31 +0800

    Add MySQL "host" parameter
    Add mysql --host option, and set "localhost" as default.

--- setup_databases.sh
+++ setup_databases.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-read -p "Please input Mysql User(default is root):" user
+read -p "Please input MySQL User(default is root):" user
 if [ "${user}" = "" ];then
     user="root"
 fi
-read -p "Please input Mysql ${user}'s password(default is ''):" passwd
-echo "drop database if exists valentine" | mysql --user=${user} --password=${passwd}
-echo "create database valentine" | mysql --user=${user} --password=${passwd}
+
+read -p "Please input MySQL Host(default is localhost):" host
+if [ "${host}" = "" ];then
+    host="localhost"
+fi
+
+read -p "Please input MySQL ${user}'s password(default is ''):" passwd
+
+echo "drop database if exists valentine" | mysql --host=${host} --user=${user} --password=${passwd}
+echo "create database valentine" | mysql --host=${host} --user=${user} --password=${passwd}
 if [ $? -ne 0 ]; then
     exit 1
 fi
-(echo "use valentine"; cat vilya/databases/schema.sql) | mysql --user=${user} --password=${passwd}
+(echo "use valentine"; cat vilya/databases/schema.sql) | mysql --host=${host} --user=${user} --password=${passwd}
